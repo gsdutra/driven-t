0e46af68c8e53479869850fea0724ca99b661acb
gabrielsd <gabrielsprecherdutra@gmail.com>
2023-04-10 16:54:23 -0300

    fix: result.data.erro fixed

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -6,10 +6,11 @@ import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enr
 import { exclude } from '@/utils/prisma-utils';
 
 async function getAddressFromCEP(cep: string) {
-  if (Number.isNaN(Number(cep))) throw invalidCepError();
+  if (Number.isNaN(Number(cep)) || cep.length !== 8) throw invalidCepError();
 
   const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
-  if (!result.data) {
+
+  if (!result.data || result.data.erro === 'true') {
     throw notFoundError();
   }
 
